528b060a7131450efef85472276625789d23e103
mayis25 <mayismikaelyan582gmail.com>
2026-02-24 13:45:19 +0300

    полиндром апдейт

--- srezlogicisemetrii.py
+++ srezlogicisemetrii.py
@@ -14,10 +14,20 @@ print(s[len(s) - 1 - i])    # v
 
 #Задание
 
-f = input("Слово: ")
-a = f[::-1]
+f = input("Слово: ").lower()
+g = int(len(f))
+e = list(f)
+h = g
+j = 0
 
-if a == f:
-    print("Полиндром")
-else:
-    print("Не полиндром")
+for i in range(g):
+    if e[h-g] == e[g-1]:
+        print("Подходит буква")
+        g -= 1
+        j += 1
+    else:
+        print("Не полиндром")
+    if j == h:
+        print("Полиндром")
+
+
