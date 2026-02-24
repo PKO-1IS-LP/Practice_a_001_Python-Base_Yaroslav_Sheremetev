9714e7b5b6067c86d58a2377776f29d9efaabbbc
mayis25 <mayismikaelyan582gmail.com>
2026-02-24 14:08:37 +0300

    Таск 1 завершон апдейт

---
+++ 17.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5]
+
+for num in numbers:
+    print(num)
+
+
+print("--------------------------")
+
+numbers = [1, 2, 3, 4, 5]
+
+for i in range(len(numbers)):  
+    print(numbers[i])
---
+++ task1.py
@@ -0,0 +1,20 @@
+
+
+s = int(input("Число:"))
+chislachet = []
+chislanechet = []
+chet = 0
+nechet = 0
+
+for q in range(s):
+    if s%2 == 0:
+        chislachet.append(s)
+        s -= 1
+        chet += 1
+    else:
+        chislanechet.append(s)
+        s -= 1
+        nechet += 1
+
+print(f"""{chislachet} Чётных: {chet}""")
+print(f"""{chislanechet} Нечётных: {nechet}""")
