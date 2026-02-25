7d81333479455cec07481588446e127416ae29a6
mayis25 <mayismikaelyan582gmail.com>
2026-02-25 10:39:56 +0300

    Лайт таски  апдейт

---
+++ litetask3.py
@@ -0,0 +1,9 @@
+s = int(input("Натуральное число:"))
+
+w = str(s)
+
+d = list(w)
+
+e = (d[-1::1])
+
+print(e)
---
+++ task4.py
@@ -0,0 +1,21 @@
+s = int(input("Введите количество чисел: "))
+numbers = list(map(int, input("Введите числа через пробел: ").split()))
+
+positives = []
+positive = 0
+negative = 0
+zero = 0
+
+for num in numbers[:s]:  
+    if num > 0:
+        positive += 1
+        positives.append(numbers[s:s+1])
+    elif num < 0:
+        negative += 1
+    else:
+        zero += 1
+
+print(f"Положительных: {positive}")
+print(f"Отрицательных: {negative}")
+print(f"Нулей: {zero}")
+print(positives)
