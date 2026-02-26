46a0b06f0cf0bbcf3eb24fe6be2e8bc83707b421
mayis25 <mayismikaelyan582gmail.com>
2026-02-26 09:34:23 +0300

    geoda1, geoda2 update

---
+++ geoda2.py
@@ -0,0 +1,18 @@
+x1 = input("X: ")
+y1 = input("Y: ")
+
+x = float(x1)
+y = float(y1)
+
+def IsPointInSquare(x, y):
+    return abs(x)<1 and abs(y)<1
+
+def main():
+    if IsPointInSquare(x, y) or (y+x) == 1:
+        print("YES")
+    else:
+        print("NO")
+    return 
+
+
+main()
